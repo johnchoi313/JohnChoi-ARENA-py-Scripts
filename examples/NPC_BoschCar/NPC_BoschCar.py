499b8679f843135fd822bced3456be64f8b50572
John Choi <johnchoi313@gmail.com>
2023-11-07 18:52:48 -0500

    fixed bosch car path

--- examples/NPC_BoschCar/NPC_BoschCar.py
+++ examples/NPC_BoschCar/NPC_BoschCar.py
@@ -288,5 +288,36 @@ def Speech_Handler(): #iteratively adds characters to speech bubble
         #if(npc.bubbles.speechBubble.data.text != npc.bubbles.speech):
         scene.update_object(npc.bubbles.speechBubble)
         
-        
+
+
+#Added laser pointer into this script:
+def click(scene, evt, msg):
+    if evt.type == "mousedown":
+        # print("Click!")
+        start = evt.data.clickPos
+        end = evt.data.position
+        start.y=start.y-.1
+        start.x=start.x-.1
+        start.z=start.z-.1
+        line = ThickLine(path=(start,end), color=(255,0,0), lineWidth=5, ttl=1)
+        scene.add_object(line)
+        ball = Sphere(
+            position=end,
+            scale = (0.06,0.06,0.06),
+            color=(255,0,0),
+            ttl=1)
+        scene.add_object(ball)
+
+@scene.run_once
+def main():
+    objs = scene.get_persisted_objs()
+    for obj_id,obj in objs.items():
+        # obj.update_attributes(clickable=True)
+        if obj.clickable:
+            obj.update_attributes(evt_handler=click)
+            scene.update_object(obj)
+            print(obj)
+
+
+
 scene.run_tasks()
